8f0fa3fc78a89a473b0bf1d3ab3d2e1644e70c8b
Gabriel <gabrielsantoscimol@gmail.com>
2020-05-24 11:34:45 -0300

    Allow to add new host to dns server (#6)
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- dnsserver/files/init.sh
+++ dnsserver/files/init.sh
@@ -9,20 +9,23 @@ cp /root/named.conf.local /etc/bind/named.conf.local
 cp /root/named.conf.options /etc/bind/named.conf.options
 cp /root/reverse.example.local /etc/bind/reverse.$DEFAULT_DNS_DOMAIN
 
-sed -i "s/DEFAULT_DNS_DOMAIN/$DEFAULT_DNS_DOMAIN/g" /etc/bind/*
+sed -i "s/{DEFAULT_DNS_DOMAIN}/$DEFAULT_DNS_DOMAIN/g" /etc/bind/*
 
 HOST_IP=`ifconfig eth0 |egrep netmask |sed -E "s/[a-zA-Z]*//g" |sed "s/   /|/g" |cut -d "|" -f4`
-sed -i "s/HOST_IP/$HOST_IP/g" /etc/bind/*
+HOST_IP_LAST_OCTECT=`echo $HOST_IP |sed -E "s/^([0-9]{1,3}.){3}//g"`
+HOST_IP_FIRST_OCTECTS=`echo $HOST_IP |sed -E "s/\.[0-9]{1,3}$//g"`
+REV_HOST_IP_FIRST_OCTECTS=`echo $HOST_IP_FIRST_OCTECTS | sed -E "s/^([0-9]{1,3}.){2}//g"`.`echo $HOST_IP_FIRST_OCTECTS | sed -E "s/^[0-9]{1,3}.//g" |sed -E "s/.[0-9]{1,3}$//g"`.`echo $HOST_IP_FIRST_OCTECTS | sed -E "s/(.[0-9]{1,3}){2}$//g"`
+
+sed -i "s/{HOST_IP}/$HOST_IP/g" /etc/bind/*
+sed -i "s/{HOST_IP_LAST_OCTECT}/$HOST_IP_LAST_OCTECT/g" /etc/bind/*
+sed -i "s/{HOST_IP_FIRST_OCTECTS}/$HOST_IP_FIRST_OCTECTS/g" /etc/bind/*
+sed -i "s/{REV_HOST_IP_FIRST_OCTECTS}/$REV_HOST_IP_FIRST_OCTECTS/g" /etc/bind/*
 
 echo DEFAULT_DNS_DOMAIN: $DEFAULT_DNS_DOMAIN
 echo HOST_IP: $HOST_IP
+echo HOST_IP_FIRST_OCTECTS: $HOST_IP_FIRST_OCTECTS
+echo REV_HOST_IP_FIRST_OCTECTS: $REV_HOST_IP_FIRST_OCTECTS
 
-/usr/sbin/named
-while sleep 2; do
-	ps aux |grep named |grep -q -v grep
-	if [ $? -ne 0 ];
-	then
-		echo "Bind process has already exited."
-		exit 1
-	fi
-done
+service named start
+
+tail -f /dev/null
