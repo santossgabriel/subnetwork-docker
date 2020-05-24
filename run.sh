ac83f5e4c9d84f73167ee8b43629f6f7af56f1e6
Gabriel Santos <gabrielsantoscimol@gmail.com>
2020-05-24 16:01:05 -0300

    Add compose file
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker network create --subnet=172.20.20.0/24 in-lab
 docker network create --subnet=172.30.30.0/24 ex-lab
-docker run -d --name mail --net in-lab --ip 172.20.20.10 mail:1.0 
-docker run -d --name dns -e DEFAULT_DNS_DOMAIN=test.lab --net in-lab --ip 172.20.20.20 dns:1.0 
-docker run -it --name client1 --net in-lab --dns 172.20.20.20 ubuntu:1.0 /bin/bash
+docker run -d --name mail --net in-lab --ip 172.20.20.10 mail:1.0
+docker run -d --name dns -e DEFAULT_DNS_DOMAIN=test.lab --net in-lab --ip 172.20.20.20 dns:1.0
+docker run -it --name client1 --net in-lab --dns 172.20.20.20 core:1.0 /bin/bash
--- webmail/files/init.sh
+++ webmail/files/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-service dovecot restart
-service postfix restart
+echo "***** Squirrel Webmail Server *****"
+service dovecot start
+service postfix start
 mkdir -p /var/run/apache2
+HOST_IP=`ifconfig eth0 |egrep netmask |sed -E "s/[a-zA-Z]*//g" |sed "s/   /|/g" |cut -d "|" -f4`
+echo ServerName $HOST_IP >> /etc/apache2/apache2.conf 
 source /etc/apache2/envvars
-echo "** Starting Apache **"
+echo "* Starting Apache HTTP Server"
 exec /usr/sbin/apache2 -D "FOREGROUND"
