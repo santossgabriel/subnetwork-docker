c7c9ca77c6a774342af8f88f42101492dd4253fb
Gabriel Santos <gabrielsantoscimol@gmail.com>
2020-06-02 02:37:30 -0300

    Salvar usuário e enviar email com a senha gerada
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- site/files/Controllers/ContactController.cs
+++ site/files/Controllers/ContactController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Site.Config;
 using Site.Models;
@@ -9,11 +10,17 @@ namespace Site.Controllers
   {
     private readonly MailService _mailService;
 
+    private readonly UserService _userService;
+
     private readonly AppConfig _config;
 
-    public ContactController(MailService mailService, AppConfig config)
+    public ContactController(MailService mailService,
+      AppConfig config,
+      UserService userService
+    )
     {
       _mailService = mailService;
+      _userService = userService;
       _config = config;
     }
 
@@ -22,15 +29,20 @@ namespace Site.Controllers
     [HttpPost]
     public IActionResult SendMail(ContactModel model)
     {
+      var user = new UserModel();
+      user.Email = model.Email;
+      user.Name = model.Name;
+      user.Password = Guid.NewGuid().ToString().Substring(0, 8);
+      _userService.Save(user);
+
       var email = new EmailModel();
-
-      email.From = _config.Email.Sender;
-      email.To = _config.Email.Contact;
-
-      email.Subject = "Contato";
-      email.Body = $"{model.Message}\n\n\nEmail para contato:\n {model.Email}";
+      email.From = _config.Email.Contact;
+      email.To = model.Email;
+      email.Subject = "Senha de acesso";
+      email.Body = $"Olá {model.Name}, recebemos seu contato e já preparamos um usuário pra você. Basta acessar nosso site com o seu email utilizando a seguinte senha:\n{user.Password}\n\nO grupo Fake Bank agradece sua escolha!";
 
       model.SendError = !_mailService.Send(email);
+
       return View("Index", model);
     }
   }
