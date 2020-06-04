51a4e39c7b9b64822bedfabda521e9d24cbde1ff
Gabriel <gabrielsantoscimol@gmail.com>
2020-06-04 18:20:18 -0300

    Correção do build das imagens (#45)
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- webmail/files/init.sh
+++ webmail/files/init.sh
@@ -5,7 +5,6 @@ mkdir -p /var/run/apache2
 HOST_IP=`ifconfig eth0 |egrep netmask |sed -E "s/[a-zA-Z]*//g" |sed "s/   /|/g" |cut -d "|" -f4`
 echo ServerName $HOST_IP >> /etc/apache2/apache2.conf 
 source /etc/apache2/envvars
-echo "* Starting Apache HTTP Server"
 
 if [ "$DOMAIN" = "" ];
 then
@@ -59,5 +58,7 @@ then
   echo "Email gateways $EMAIL_GATEWAY"
   postmap /etc/postfix/transport
 fi
+
+echo "* Starting Apache HTTP Server"
 
 exec /usr/sbin/apache2 -D "FOREGROUND"
