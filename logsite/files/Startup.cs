7127690808e59bb910979ddc1deb6067c2c17908
Gabriel Santos <gabrielsantoscimol@gmail.com>
2020-06-30 21:03:33 -0300

    Create new site for storage logs
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

---
+++ logsite/files/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ProxyHttpsDotnet
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddHttpsRedirection(options => options.HttpsPort = 443);
+      services.AddControllers();
+      services.AddCors(p => p.AddPolicy("AllowAll", builder =>
+      {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+      }));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseCors("AllowAll");
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
