d0c9094c73f43f8cff345a40c087371ab94d12b8
Gabriel Santos <gabrielsantoscimol@gmail.com>
2020-07-07 13:38:13 -0300

    Multiple interfaces in suricata IDS
    Signed-off-by: Gabriel Santos <gabrielsantoscimol@gmail.com>

--- employee/files/bot.py
+++ employee/files/bot.py
@@ -8,23 +8,26 @@ password = os.environ.get('BOT_PASSWORD', '')
 server = os.environ.get('FTP_SERVER', 'ftp.fakebank.lab')
 timer_sec = int(os.environ.get('TIMER_SECONDS', '10'))
 
+
 def ftp_run():
-    time.sleep(timer_sec)
-    os.system('echo " ** ' + datetime.datetime.now().strftime('%H:%M:%S') + '"')
-    try:
-        ftp = ftplib.FTP(server)
-        ftp.login(user, password)
-        data = []
-        ftp.dir(data.append)
-        ftp.quit()
-        for line in data:
-            os.system('echo "- ' + line + '"')
-    except ConnectionRefusedError:
-        print('Connection refused server ' + server)
+  time.sleep(timer_sec)
+  os.system('echo " ** ' + datetime.datetime.now().strftime('%H:%M:%S') + '"')
+  try:
+    ftp = ftplib.FTP(server)
+    ftp.login(user, password)
+    data = []
+    ftp.dir(data.append)
+    ftp.quit()
+    for line in data:
+      os.system('echo "- ' + line + '"')
+  except ConnectionRefusedError:
+    print('Connection refused server ' + server)
+  except ftplib.all_errors as e:
+    print(e)
 
 if user != '' and password != '' and server != '':
-    while True:
-        ftp_run()
+	while True:
+		ftp_run()
 
 else:
     print('User or password not provided.')
